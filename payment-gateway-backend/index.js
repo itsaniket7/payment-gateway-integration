be57907bd47b52abd162b445c7aea181f6cb4e01
Aniket Sawant <47816590+itsaniket7@users.noreply.github.com>
2024-12-07 20:45:30 +0530

    Update Port

--- payment-gateway-backend/index.js
+++ payment-gateway-backend/index.js
@@ -38,7 +38,7 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const PORT = 3000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
